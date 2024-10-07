0b5d4ef3da1c7f57b510d8d0400ffe12238d3391
MD RAKIBUL HASAN <rakibulranak@MDs-MacBook-Air.local>
2024-10-08 00:18:19 +0600

    add google analytics

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,36 @@
 import Head from "next/head";
+import Script from "next/script";
 
 export const Header = ({ seo }) => {
   return (
-    <Head>
-      <title>{seo.title}</title>
-      <meta name="description" content={seo.description} />
-      <meta property="og:title" content={seo.title} />
-      <meta property="og:image" content={seo.image} />
-      <meta property="og:type" content="website" />
-      <meta property="og:description" content={seo.description} />
-    </Head>
+    <>
+      <Head>
+        <title>{seo.title}</title>
+        <meta name="description" content={seo.description} />
+        <meta property="og:title" content={seo.title} />
+        <meta property="og:image" content={seo.image} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={seo.description} />
+      </Head>
+
+      {/* Google Tag Manager Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `,
+        }}
+      />
+    </>
   );
 };
